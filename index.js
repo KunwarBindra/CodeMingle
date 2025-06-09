25da877730cb913d77743e3ec24c9877348e34bd
KunwarBindra <kunwarjeetbindra@gmail.com>
2025-06-09 00:53:11 -0700

    mongoose integration done, basic user signup apis created

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 const app = require('express')();
+const mongoose = require('mongoose');
+const User = require('./models/users');
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  res.send('Hello, World!');
-})
+app.use(require('express').json());
 
-app.listen(7777)
+app.post('/api/users', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+  const existingUser = await User.find({ email: email });
+  if (existingUser.length) {
+    return res.status(400).json({ error: 'User already exists' });
+  }
+  const newUser = new User({ firstName, lastName, email, password });
+  newUser.save()
+    .then(user => res.status(200).json(user))
+    .catch(err => {
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+app.patch('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  User.findByIdAndUpdate(id, req.body).then(user => {
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json({ message: 'User updated successfully' });
+  }).catch(err => {
+    res.status(500).json({ error: 'Internal server error' });
+  });
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  User.findById(id).then(user => {
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  }).catch(err => {
+    res.status(500).json({ error: 'Internal server error' });
+  });
+});
+
+app.get('/api/users', (req, res) => {
+  User.find().then(users => {
+    res.status(200).json(users);
+  }).catch(err => {
+    res.status(500).json({ error: 'Internal server error' });
+  });
+});
+
+mongoose.connect('mongodb+srv://kunwarjeetbindra:78Percent@cluster0.rxlpdkz.mongodb.net/codemingle?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(7777)
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
